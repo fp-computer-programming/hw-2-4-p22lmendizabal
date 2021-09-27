2bc30c646bea4dd967ff8b98b334487810298656
p22mendizabal <p22lmendizabal@fairfieldprep.org>
2021-09-26 22:48:41 -0400

    add file
    hw3-1-5complete

---
+++ hw3-1-5.py
@@ -0,0 +1,13 @@
+#author : LM (AMDG) 09/26/2021
+
+# question 5
+
+a = 1
+
+b = 3
+
+c = -4
+
+solution1 = (-b + ((b**2 - (4 * a * c))**.5)) / (2 * a)
+solution2 = (-b - ((b**2 - (4 * a * c))**.5)) / (2 * a)
+print(solution1, solution2)
