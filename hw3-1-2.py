518de656de346d55689d34a8e1d5cef466bf7fd7
p22mendizabal <p22lmendizabal@fairfieldprep.org>
2021-09-26 22:25:27 -0400

    add file
    hw3-1-2 is complete

---
+++ hw3-1-2.py
@@ -0,0 +1,10 @@
+# author = LM (AMDG) 09/26/ 2021
+
+#Question 2
+
+solution = (7 + 9) ** .5 * 2
+
+# the reciprocal of 2 is 1/2 so raising the number to 1/2 is the same as finding the square root.
+
+print (solution)
+
